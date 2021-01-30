e06ab2c34df443c4e4ac388ae800a494a25859cb
pjm <2557272503@qq.com>
2021-01-30 10:02:44 +0800

    'guli'

---
+++ common/service_base/src/main/java/com/atguigu/servicebase/exceptionhandler/GlobalExceptionHandler.java
@@ -0,0 +1,41 @@
+package com.atguigu.servicebase.exceptionhandler;
+
+import com.atguigu.commonutils.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    /*
+    异常执行过程，先找指定异常，后面找全局异常
+     */
+
+    //全局异常处理
+    @ExceptionHandler(Exception.class)
+    @ResponseBody //为了返回数据
+    public R error(Exception e) {
+        e.printStackTrace();
+        return R.error().message("执行了全局异常处理..");
+    }
+
+    //指定异常处理
+    @ExceptionHandler(ArithmeticException.class)
+    @ResponseBody //为了返回数据
+    public R error(ArithmeticException e) {
+        e.printStackTrace();
+        return R.error().message("执行了ArithmeticException异常处理..");
+    }
+
+    //自定义异常需要手动抛出
+    @ExceptionHandler(GuliException.class)
+    @ResponseBody //为了返回数据
+    public R error(GuliException e) {
+        log.error(e.getMessage());
+        e.printStackTrace();
+        return R.error().code(e.getCode()).message(e.getMsg());
+    }
+}
